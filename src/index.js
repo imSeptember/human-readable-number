29e1206040803760b2db1af9fb9df58cf912b91c
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-08-05 21:43:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
-  
+    
+const units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  const teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  const getWords = (number) => {
+    if (number < 10) {
+      return units[number];
+    } else if (number < 20) {
+      return teens[number - 10];
+    } else if number < 100) {
+      return tens[Math.floor(number / 10)] + (number % 10 !== 0 ? "-" + units[number % 10] : "");
+    } else if (number < 1000) {
+      return units[Math.floor(number / 100)] + " hundred" + (number % 100 !== 0 ? " and " + getWords(number % 100) : "");
+    } else if (number < 1000000) {
+      return getWords(Math.floor(number / 1000)) + " thousand" + (number % 1000 !== 0 ? " " + getWords(number % 1000) : "");
+    } else {
+      return "Number is too large";
+    }
+  };
+
+  if (number === 0) {
+    return "zero";
+  } else if (number < 0) {
+    return "negative " + getWords(Math.abs(number));
+  } else {
+    return getWords(number);
+  }
+
+    
 }
