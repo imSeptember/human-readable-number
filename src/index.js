7d6ea4149783093097ea23bd363ffef14b4d1cc4
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-08-05 21:32:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 module.exports = function toReadable (number) {
-  
+  const numberToWords = (num) => {
+  const numberWords = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+  };
+
+  const getWords = (num) => {
+    if (num < 20) {
+      return numberWords[num];
+    } else if (num < 100) {
+      const tensPlace = Math.floor(num / 10) * 10;
+      const onesPlace = num % 10;
+      return numberWords[tensPlace] + (onesPlace !== 0 ? "-" + numberWords[onesPlace] : "");
+    } else if (num < 1000) {
+      const hundredsPlace = Math.floor(num / 100);
+      const remainder = num % 100;
+      return numberWords[hundredsPlace] + " hundred" + (remainder !== 0 ? " and " + getWords(remainder) : "");
+    } else if (num < 1000000) {
+      const thousandsPlace = Math.floor(num / 1000);
+      const remainder = num % 1000;
+      return getWords(thousandsPlace) + " thousand" + (remainder !== 0 ? " " + getWords(remainder) : "");
+    } else {
+      return "Number is too large";
+    }
+  };
+
+  if (num < 0) {
+    return "negative " + getWords(Math.abs(num));
+  } else {
+    return getWords(num);
+  }
+};
 }
