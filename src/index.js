963bfe72a8f5099b6fada18986e3ad17abe7679a
Vladislav Kalenskiy <kalenskiyvlad@gmail.com>
2023-08-05 22:46:41 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
+    const words = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety",
+    };
 
-}
+    const getWords = (num) => {
+        if (num < 20) {
+            return words[num];
+        } else if (num < 100) {
+            const tensPlace = Math.floor(num / 10) * 10;
+            const onesPlace = num % 10;
+            return (
+                words[tensPlace] +
+                (onesPlace !== 0 ? " " + words[onesPlace] : "")
+            );
+        } else if (num < 1000) {
+            const hundredsPlace = Math.floor(num / 100);
+            const remainder = num % 100;
+            return (
+                words[hundredsPlace] +
+                " hundred" +
+                (remainder !== 0 ? " " + getWords(remainder) : "")
+            );
+        } else if (num < 1000000) {
+            const thousandsPlace = Math.floor(num / 1000);
+            const remainder = num % 1000;
+            return (
+                getWords(thousandsPlace) +
+                " thousand" +
+                (remainder !== 0 ? " " + getWords(remainder) : "")
+            );
+        }
+    };
+    return getWords(number);
+};
