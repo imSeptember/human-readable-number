a916df96034ce2ebac717ec0974f0bd485b01588
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-08-05 21:44:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,60 +1,30 @@
-module.exports = function toReadable (num) {
-  const numberToWords = (num) => {
-  const numberWords = {
-    0: "zero",
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "fifteen",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "nineteen",
-    20: "twenty",
-    30: "thirty",
-    40: "forty",
-    50: "fifty",
-    60: "sixty",
-    70: "seventy",
-    80: "eighty",
-    90: "ninety",
-  };
+module.exports = function toReadable (number) {
 
-  const getWords = (num) => {
-    if (num < 20) {
-      return numberWords[num];
-    } else if (num < 100) {
-      const tensPlace = Math.floor(num / 10) * 10;
-      const onesPlace = num % 10;
-      return numberWords[tensPlace] + (onesPlace !== 0 ? "-" + numberWords[onesPlace] : "");
-    } else if (num < 1000) {
-      const hundredsPlace = Math.floor(num / 100);
-      const remainder = num % 100;
-      return numberWords[hundredsPlace] + " hundred" + (remainder !== 0 ? " and " + getWords(remainder) : "");
-    } else if (num < 1000000) {
-      const thousandsPlace = Math.floor(num / 1000);
-      const remainder = num % 1000;
-      return getWords(thousandsPlace) + " thousand" + (remainder !== 0 ? " " + getWords(remainder) : "");
+
+const units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  const teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  const getWords = (number) => {
+    if (number < 10) {
+      return units[number];
+    } else if (number < 20) {
+      return teens[number - 10];
+    } else if number < 100) {
+      return tens[Math.floor(number / 10)] + (number % 10 !== 0 ? "-" + units[number % 10] : "");
+    } else if (number < 1000) {
+      return units[Math.floor(number / 100)] + " hundred" + (number % 100 !== 0 ? " and " + getWords(number % 100) : "");
+    } else if (number < 1000000) {
+      return getWords(Math.floor(number / 1000)) + " thousand" + (number % 1000 !== 0 ? " " + getWords(number % 1000) : "");
     } else {
       return "Number is too large";
     }
   };
 
-  if (num < 0) {
-    return "negative " + getWords(Math.abs(num));
+  if (number === 0) {
+    return "zero";
+  } else if (number < 0) {
+    return "negative " + getWords(Math.abs(number));
   } else {
-    return getWords(num);
+    return getWords(number);
   }
-};
-}
